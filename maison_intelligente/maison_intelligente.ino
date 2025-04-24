f3e38570ab9b7ea3e481aaaeef43a2fa8fe888c7
Maskaroni <MiEveryone@outlook.com>
2025-04-23 21:26:57 -0400

    Labo 6 sans refactorisation

--- maison_intelligente/maison_intelligente.ino
+++ maison_intelligente/maison_intelligente.ino
@@ -1,3 +1,4 @@
+#include <U8g2lib.h>
 #include <LiquidCrystal.h>
 #include <LCD_I2C.h>
 #include <AccelStepper.h>
@@ -9,6 +10,9 @@
 #define IN_2 3
 #define IN_3 4
 #define IN_4 5
+#define CLK_PIN 22
+#define DIN_PIN 26
+#define CS_PIN  24
 const int RED_PIN = 8;
 const int BLUE_PIN = 9;
 const int ALARM = 13;
@@ -16,6 +20,7 @@ const int ALARM = 13;
 LCD_I2C lcd(0x27, 16, 2);
 HCSR04 hc(TRIGGER_PIN, ECHO_PIN);
 AccelStepper motor(MOTOR_INTERFACE_TYPE, IN_1, IN_3, IN_2, IN_4);
+U8G2_MAX7219_8X8_F_4W_SW_SPI u8g2(U8G2_R0, CLK_PIN, DIN_PIN, CS_PIN, U8X8_PIN_NONE, U8X8_PIN_NONE);
 
 unsigned long currentTime = 0;
 
@@ -23,14 +28,15 @@ int distance;
 int degre;
 int aimingAt;
 const int zero = 0;
-const int maxNear = 30;
-const int maxFar = 60;
+int maxNear = 30;
+int maxFar = 60;
 const int motorLeft = 0;
 const int motorRight = 1024;
 const int left = 10;
 const int right = 170;
-const int alarmTreshold = 15;
+int alarmTreshold = 15;
 bool changeAlarmState = false;
+bool isMatrixOn = false;
 
 
 //SETUP AND LOOP
@@ -52,6 +58,11 @@ void setup() {                                                                  
   pinMode(RED_PIN, OUTPUT);
   pinMode(BLUE_PIN, OUTPUT);
 
+  u8g2.begin();
+  u8g2.setContrast(7);
+  u8g2.clearBuffer();
+  u8g2.sendBuffer(); 
+
   Serial.println("Setup completed!");
 
 
@@ -68,31 +79,124 @@ void loop() {                                                                   
   currentTime = millis();
 
   getDistance(currentTime);
-  refreshScreenPlusStats(currentTime);
+  refreshScreen(currentTime);
   aimingStates();
   alarmStates(currentTime);
+  commands(currentTime);
 }
 
+//COMMANDS
+void commands(unsigned long currentTime) {
+  enum Icon {NO_ICON, CHECK, ERROR, LIMIT_CROSSED};
+  static Icon whichIcon = NO_ICON;
+  display(currentTime, whichIcon);
+  String cmd = "";
+  int slicedCmd;
 
-//REFRESH AND DISTANCE
-void refreshScreenPlusStats(unsigned long ct) {                                                 //refreshScreenPlusStats
-  unsigned static long timer;
-  static bool timerIsDone = false;
+  if (Serial.available() > 0) {
+    cmd = Serial.readStringUntil('\n');
 
-  if (!timerIsDone) {
-    timer = ct + 100;
-    timerIsDone = true;
+    if (cmd == "gDist") {
+      Serial.println(distance);
+      isMatrixOn = true;
+      whichIcon = CHECK;
+    }
+    else if (cmd.startsWith("cfg;alm;")) {
+      cmd = cmd.substring(8);
+      alarmTreshold = cmd.toInt();
+      isMatrixOn = true;
+      whichIcon = CHECK;
+    }
+    else if (cmd.startsWith("cfg;lim_")) {
+      isMatrixOn = true;
+      whichIcon = processLimitsMotor(cmd);
+    }
+    else {
+      Serial.println("Arduino a compris: '" + cmd + "'! Ce n'est pas une commande valide, recommencez.");
+      isMatrixOn = true;
+      whichIcon = ERROR;
+    }
+  }
+}
+
+int processLimitsMotor(String cmd) {
+  int pastLimit;
+  int icon;
+
+  if (cmd.startsWith("cfg;lim_inf;")) {
+    pastLimit = maxNear;
+
+    cmd = cmd.substring(13);
+    maxNear = cmd.toInt();
+
+    icon = 1;
+  }
+  else if (cmd.startsWith("cfg;lim_sup;")) {
+    pastLimit = maxFar;
+
+    cmd = cmd.substring(13);
+    maxFar = cmd.toInt();
+
+    icon = 1;
+  }
+  else {
+    Serial.println("Arduino a compris: '" + cmd + "'! Ce n'est pas une commande valide, recommencez.");
+    icon = 2;
   }
 
+  if (maxNear >= maxFar) {
+    Serial.println("  ERREUR! \n Limite inférieure plus grande que limite supérieure.");
+    maxNear = pastLimit;
+    icon = 3;
+  }
+
+  return icon;
+}
+
+void display(unsigned long ct, int whichOne) {
+  unsigned static long timer;
+  static bool isTimerStarted = false;
+
+  if (!isTimerStarted && isMatrixOn) {
+    u8g2.clearBuffer();
+    switch (whichOne) {
+      case 1:
+        u8g2.drawLine(6, 7, 1, 2);
+        u8g2.drawLine(2, 1, 3, 0);
+        break;
+      case 2:
+        u8g2.drawLine(0, 0, 7, 7);
+        u8g2.drawLine(7, 0, 0, 7);
+        break;
+      case 3:
+        u8g2.drawCircle(4, 3, 3);
+        u8g2.drawLine(7, 0, 0, 7);
+        break;
+    }
+    u8g2.sendBuffer(); 
+    timer = ct + 3000;
+    isTimerStarted = true;
+  }
   if (ct >= timer) {
-    Serial.print("etd:2486739,dist:");
-    Serial.print(distance);
-    Serial.print(",deg:");
-    Serial.println(degre);
+    isMatrixOn = false;
+    isTimerStarted = false;
+    u8g2.clearBuffer();
+    u8g2.sendBuffer();
+  }
+}
 
+//REFRESH AND DISTANCE
+void refreshScreen(unsigned long ct) {                                                          //refreshScreen
+  unsigned static long timer;
+  static bool isTimerStarted = false;
+
+  if (!isTimerStarted) {
+    timer = ct + 100;
+    isTimerStarted = true;
+  }
+  if (ct >= timer) {
     screen();
-
-    timerIsDone = false;
+    isTimerStarted = false;
   }
 }
 
@@ -188,9 +292,9 @@ void motorOff() {                                                               
 }
 
 void stopTheSound() {                                                                           //stopTheSound
-  noTone(ALARM);
+  digitalWrite(ALARM, zero);
 
-  if (distance <= alarmTreshold) {
+  if (distance <= alarmTreshold && distance != zero) {
     changeAlarmState = true;
   }
 }
@@ -214,7 +318,7 @@ void makeSound(unsigned long currentTime) {                                     
     isTimerStarted = false;
   }
 
-  tone(ALARM, 500);
+  digitalWrite(ALARM, 255);
 }
 
 void policeLights(unsigned long currentTime) {                                                  //policeLights
